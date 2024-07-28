2af5c1bca31abe924b7182f68ad9c612e7542eea
Sam O'Ferrell <saoferre@ncsu.edu>
2024-07-28 14:54:30 -0400

    adding necessary files

--- myAPI.R
+++ myAPI.R
@@ -115,14 +115,20 @@ data_factors <- data |>
                                 "Less than $50,000",
                                 "Less than $75,000",
                                 "More than $75,000")))
-model3 <- Diabetes_binaryF ~ HvyAlcoholConsumpF + GenHlthF + PhysHlthF + MentHlthF + SmokerF + HighCholF
+model3 <- Diabetes_binaryF ~ HvyAlcoholConsumpF + GenHlthF + PhysHlthF + MentHlthF + SmokerF + HighCholF + BMI
 fit <- glm(model3, data = data, family = "binomial")
 ####################################### adding API stuff below ###################################
 
-#* @get /mean
+#* @get /mode
 #* @param predictor
 test <- function(predictor, data = data_factors){
-    mode <- Mode(data_factors$predictor)
-    return(mode[1])
-
+  if (is.factor(data_factors[[predictor]])){
+    mode <- Mode(data_factors[[predictor]])
+    value <- as.character(mode[[1]])
+    return(cat("The most prevalent value for", predictor, "is", value))
+  }
+  if (!is.factor(data_factors[[predictor]])){
+    mean_value <- mean(data_factors[[predictor]])
+    return(cat("The mean of", predictor, "is", mean_value))  
+  }
   }
