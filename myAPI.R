e523502135190b167a179be5d88121f54e80b691
Sam O'Ferrell <saoferre@ncsu.edu>
2024-07-29 12:45:59 -0400

    getting ready to render

--- myAPI.R
+++ myAPI.R
@@ -2,23 +2,14 @@ library(tidyverse)
 library(leaflet)
 library(DescTools) # for getting mode
 data <- readRDS("data_factors.rds")
-#fit <- glm(Diabetes_binaryF ~ HvyAlcoholConsumpF + PhysHlthF + MentHlthF + SmokerF + HighCholF + BMI, data = data, family = "binomial")
+fit <- glm(Diabetes_binaryF ~ HvyAlcoholConsumpF + PhysHlthF + MentHlthF + SmokerF + HighCholF + BMI, data = data, family = "binomial")
 ####################################### adding API stuff below ###################################
 
-#*Analyzing Predictors
+# #*Analyzing Predictors
+
 #* @get /pred
-#* @param predictor
-function(predictor, data = data){
-  if (is.factor(data[[predictor]])){
-    mode <- Mode(data[[predictor]])
-    value <- as.character(mode[[1]])
-    return(paste("The most prevalent value for", predictor, "is", value))
-  }
-  if (!is.factor(data[[predictor]])){
-    mean_value <- mean(data[[predictor]])
-    return(paste("The mean of", predictor, "is", mean_value))  
-  }
-}
+#* 
+
 #http://localhost:PORT/pred?predictor=PhysHlthF
 #http://localhost:PORT/pred?predictor=BMI
 #http://localhost:PORT/pred?predictor=SmokerF
